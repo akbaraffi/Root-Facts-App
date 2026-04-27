6997995382dde292e24263637157c3e544f377ee
Akbar <akbarraffi100@gmail.com>
2026-04-27 21:59:11 +0700

    version 1.3

--- src/sw.js
+++ src/sw.js
@@ -3,25 +3,29 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox
 if (workbox) {
   console.log('Workbox berhasil dimuat');
 
+  self.skipWaiting();
+  workbox.core.clientsClaim();
+  workbox.precaching.cleanupOutdatedCaches();
+
   workbox.precaching.precacheAndRoute([
-    { url: '/', revision: '1.0.0' },
-    { url: '/index.html', revision: '1.0.0' },
-    { url: '/app.bundle.js', revision: '1.0.0' },
-    { url: '/app.bundle.css', revision: '1.0.0' },
-    { url: '/manifest.json', revision: '1.0.0' },
-    { url: '/favicon.ico', revision: '1.0.0' },
-    { url: '/icons/icon-192x192.png', revision: '1.0.0' },
-    { url: '/icons/icon-512x512.png', revision: '1.0.0' },
-    { url: '/icons/apple-touch-icon.png', revision: '1.0.0' },
-    { url: '/screenshots/desktop.png', revision: '1.0.0' },
-    { url: '/screenshots/mobile.png', revision: '1.0.0' },
-    { url: '/model/model.json', revision: '1.0.0' },
-    { url: '/model/metadata.json', revision: '1.0.0' },
-    { url: '/model/weights.bin', revision: '1.0.0' },
-    { url: 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.22.0/dist/tf.min.js', revision: '1.0.0' },
-    { url: 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-webgpu@4.22.0/dist/tf-backend-webgpu.min.js', revision: '1.0.0' },
-    { url: 'https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.8.1', revision: '1.0.0' },
-    { url: 'https://unpkg.com/lucide@0.462.0/dist/umd/lucide.js', revision: '1.0.0' },
+    { url: '/', revision: '1.0.1' },
+    { url: '/index.html', revision: '1.0.1' },
+    { url: '/app.bundle.js', revision: '1.0.1' },
+    { url: '/app.bundle.css', revision: '1.0.1' },
+    { url: '/manifest.json', revision: '1.0.1' },
+    { url: '/favicon.ico', revision: '1.0.1' },
+    { url: '/icons/icon-192x192.png', revision: '1.0.1' },
+    { url: '/icons/icon-512x512.png', revision: '1.0.1' },
+    { url: '/icons/apple-touch-icon.png', revision: '1.0.1' },
+    { url: '/screenshots/desktop.png', revision: '1.0.1' },
+    { url: '/screenshots/mobile.png', revision: '1.0.1' },
+    { url: '/model/model.json', revision: '1.0.1' },
+    { url: '/model/metadata.json', revision: '1.0.1' },
+    { url: '/model/weights.bin', revision: '1.0.1' },
+    { url: 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.22.0/dist/tf.min.js', revision: '1.0.1' },
+    { url: 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-webgpu@4.22.0/dist/tf-backend-webgpu.min.js', revision: '1.0.1' },
+    { url: 'https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.8.1', revision: '1.0.1' },
+    { url: 'https://unpkg.com/lucide@0.462.0/dist/umd/lucide.js', revision: '1.0.1' },
   ]);
 
   workbox.routing.registerRoute(
